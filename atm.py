132b1b677cfd51bc3e1e529f4b05f3c17ca02fc9
zejuniortdr <zejuniortdr@gmail.com>
2025-05-06 12:18:12 -0300

    initial

---
+++ atm.py
@@ -0,0 +1,9 @@
+"""
+Implemente uma função que simule o saque de um caixa eletrônico.
+O objetivo é retornar a menor quantidade possível de notas, priorizando as de maior valor primeiro.
+
+Regras:
+As notas disponíveis são: 5, 10, 20, 50 e 100.
+A função deve retornar as notas que compõem o valor solicitado.
+
+"""
