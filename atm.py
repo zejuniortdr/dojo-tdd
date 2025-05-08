71514de7ad1db9d816625961eaaeb82e2b3358d9
zejuniortdr <zejuniortdr@gmail.com>
2025-05-08 11:50:41 -0300

    enunciado

--- atm.py
+++ atm.py
@@ -1,9 +1,22 @@
-"""
-Implemente uma função que simule o saque de um caixa eletrônico.
-O objetivo é retornar a menor quantidade possível de notas, priorizando as de maior valor primeiro.
+""""
+Caixa Eletrônico
+Desenvolva um programa que simule a entrega de notas quando um cliente efetuar um saque em um caixa eletrônico.
 
-Regras:
-As notas disponíveis são: 5, 10, 20, 50 e 100.
-A função deve retornar as notas que compõem o valor solicitado.
+Requisitos:
 
+- Entregar o menor número de notas;
+- É possível sacar o valor solicitado com as notas disponíveis;
+- Saldo do cliente infinito;
+- Quantidade de notas infinito;
+- Notas disponíveis de 100,00; R$ 50,00; R$ 20,00, R$ 10,00 e R$ 5,00
+
+Exemplos:
+Valor do Saque: R$ 50,00
+Resultado Esperado: Entregar uma nota de R$ 50,00.
+
+Valor do Saque: R$ 30,00
+Resultado Esperado: Entregar uma nota de R$ 20,00 e uma de R$ 10,00.
+
+Valor do Saque: R$ 80,00
+Resultado Esperado: Entregar uma nota de R$ 50,00 e uma de R$ 20,00 e uma de R$ 10,00.
 """
